24a0a12697f7da839480775d46e0c0a4c7bb3587
orkhank <118054233+orkhank@users.noreply.github.com>
2024-03-29 18:36:32 +0300

    Refactor image similarity metrics in utils.py

--- utils.py
+++ utils.py
@@ -3,7 +3,6 @@ from skimage.metrics import (
     structural_similarity as ssim,
     peak_signal_noise_ratio as psnr,
 )
-import cv2 as cv
 
 
 def mean_match(image1, image2) -> float:
@@ -21,21 +20,41 @@ def mean_squared_error(image1, image2) -> float:
 
 
 def cross_correlation(image1, image2) -> float:
+    """
+    Cross-correlation similarity metric.
+    Args:
+        image1 (np.ndarray): Binary black and white image.
+        image2 (np.ndarray): Binary black and white image.
+
+    Returns:
+        float: Cross-correlation similarity metric.
+    """
+
     assert image1.shape == image2.shape
     normalized_image1 = image1 / 255
     normalized_image2 = image2 / 255
 
-    Crr = np.sum(np.multiply(normalized_image1, normalized_image2))
-    norm = np.sqrt(
-        np.sum(np.square(normalized_image1)) * np.sum(np.square(normalized_image2))
+    cross_correlation = np.sum(
+        (normalized_image1 - np.mean(normalized_image1))
+        * (normalized_image2 - np.mean(normalized_image2))
+    ) / (
+        np.sqrt(
+            np.sum((normalized_image1 - np.mean(normalized_image1)) ** 2)
+            * np.sum((normalized_image2 - np.mean(normalized_image2)) ** 2)
+        )
+        + 1e-10
     )
-    return Crr / norm
+    # Normalize to [0, 1]
+    cross_correlation = (cross_correlation + 1) / 2
+    return cross_correlation
 
 
 def structural_similarity(image1, image2) -> float:
     assert image1.shape == image2.shape
 
-    return ssim(image1, image2, data_range=255)
+    score = ssim(image1, image2, data_range=255)
+    # Normalize to [0, 1]
+    return (score + 1) / 2
 
 
 def peak_signal_noise_ratio(image1, image2) -> float:
